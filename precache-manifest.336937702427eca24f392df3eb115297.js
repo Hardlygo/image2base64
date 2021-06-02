f43a3adbd96e8ad7f0430e42eb787ceacf63ac61
Hardlygo <961332109@qq.com>
2021-06-02 12:47:57 +0800

    Updates

---
+++ precache-manifest.336937702427eca24f392df3eb115297.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36b700cd94354bfff83b",
+    "url": "css/app.c78e51b6.css"
+  },
+  {
+    "revision": "763d1af85b1a5b517485",
+    "url": "css/chunk-vendors.caacd1e0.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "4576fc8cd089081376e7ad4f94f94b47",
+    "url": "index.html"
+  },
+  {
+    "revision": "36b700cd94354bfff83b",
+    "url": "js/app.d3cc67fd.js"
+  },
+  {
+    "revision": "763d1af85b1a5b517485",
+    "url": "js/chunk-vendors.21dd3890.js"
+  },
+  {
+    "revision": "1d1c12ee4662a3a0bdb68f27dfe463c2",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
